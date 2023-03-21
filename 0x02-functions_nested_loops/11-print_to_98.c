adf5ddbc52a729c336355e18e914dff46075d6ce
peemonny <peemonnyh@gmail.com>
2023-03-21 01:53:00 -0700

    up

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,34 @@
-#include "main.h"
+#include <stdio.h>
+	#include "main.h"
 
 
 	/**
-	 * _abs - computes the absolute value of an integer
-	 * @n: the int to check
-	 * Return: the absolute value of int
+	 * print_to_98 - prints all natural numbers from n to 98,
+	 * followed by a new line
+	 * @n: print from this number
 	 */
-	int _abs(int n)
+	void print_to_98(int n)
 	{
-		if (n >= 0)
+		int i, j;
+
+
+		if (n <= 98)
 		{
-			return (n);
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+			}
+		} else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
 		}
-		return (-n);
 	}
